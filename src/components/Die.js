b2d54aa172314aaf853d80706e3c91ea9a6c2f6a
Christian <christianyip92@gmail.com>
2022-10-12 15:03:48 -0700

    Add dynamic style for when die held.

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,10 @@
 export default function Die (props) {
+  let styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+  }
+  
   return (
-    <div className="die">
+    <div className="die" style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   )
